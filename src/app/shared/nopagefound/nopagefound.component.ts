85369845c608d7dbb9ab9a573ef20a2707a104a9
Matias-Espinoza <mespinoza@ubicomp-labs.com>
2018-07-01 23:42:50 -0400

    Se creo el page no found - error 404

--- src/app/shared/nopagefound/nopagefound.component.ts
+++ src/app/shared/nopagefound/nopagefound.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: []
+  styleUrls: ['nopagefound.component.css']
 })
 export class NopagefoundComponent implements OnInit {
 
